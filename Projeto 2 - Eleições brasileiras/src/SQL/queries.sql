0c0adbeef02778b33c758bb9551e26f584c55d09
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-13 16:57:31 -0400

    12/01

---
+++ Projeto 2 - Eleições brasileiras/src/SQL/queries.sql
@@ -0,0 +1,15 @@
+select 
+	trial_instituto_8 as Instituto,
+	ano as Ano_pesq,
+	cargo as Cargo_Pretendido,
+	sigla_uf is null as sigla,
+	tipo as tipo_pesquisa,
+	turno as Primeiro_Turno,
+	trial_descricao_cenario_19  as cenario
+from microdados m 
+where trial_instituto_8 = 'Datafolha' 
+and ano = 2022 
+and cargo ='presidente' 
+and tipo = 'estimulada'
+and turno =1
+and trial_descricao_cenario_19 = 'cenário 1'
---
+++ Projeto 2 - Eleições brasileiras/src/python/pesquisa.py
@@ -0,0 +1,6 @@
+import basedosdados as bd
+import pandas as pd
+import colab as df
+
+
+df.head()
