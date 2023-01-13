146c1fa0e3f0857744a6ea211d2e6823ca722144
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-13 15:29:26 -0400

    12/01

--- Projeto 2 - Eleições brasileiras/src/python/upload_data.py
+++ Projeto 2 - Eleições brasileiras/src/python/upload_data.py
@@ -1,8 +1,12 @@
 import os 
+import pandas as pd
 
 BASE_DIR = (os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 DATA_DIR = os.path.join(BASE_DIR,'data')
 
-files_names = os.listdir(DATA_DIR)
-print(files_names)
+files_names = [i for i in os.listdir(DATA_DIR) if i.endswith('.csv')]
+
+for i in files_names:
+    df_tmp = pd.read_csv(os.path.join(DATA_DIR,i))
+
 
