dca1e942d34daf6f70bbf22090d66c6e8fcd8a00
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-13 14:50:21 -0400

    13/01

--- Projeto 2 - Eleições brasileiras/src/python/upload_data.py
+++ Projeto 2 - Eleições brasileiras/src/python/upload_data.py
@@ -3,6 +3,6 @@ import os
 BASE_DIR = (os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 DATA_DIR = os.path.join(BASE_DIR,'data')
 
-print(BASE_DIR)
-print(DATA_DIR)
+files_names = os.listdir(DATA_DIR)
+print(files_names)
 
