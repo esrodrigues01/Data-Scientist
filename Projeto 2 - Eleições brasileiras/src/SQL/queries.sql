ffbd0c08d35363e79f290cf2b742d2f4d7bbc8c0
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-16 16:12:26 -0400

    16/01

--- Projeto 2 - Eleições brasileiras/src/SQL/queries.sql
+++ Projeto 2 - Eleições brasileiras/src/SQL/queries.sql
@@ -5,11 +5,18 @@ select
 	sigla_uf is null as sigla,
 	tipo as tipo_pesquisa,
 	turno as Primeiro_Turno,
-	trial_descricao_cenario_19  as cenario
+	trial_descricao_cenario_19  as cenario,
+	nome_candidato as Candidato
 from microdados m 
 where trial_instituto_8 = 'Datafolha' 
 and ano = 2022 
 and cargo ='presidente' 
 and tipo = 'estimulada'
-and turno =1
+and turno = 1 
+and nome_candidato = 'Bolsonaro' or nome_candidato = 'Lula' or nome_candidato = 'Ciro' or nome_candidato = 'Simone Tebet'
 and trial_descricao_cenario_19 = 'cenário 1 - estimulado - 1º turno'
+
+
+
+
+select nome_candidato  from microdados m where trial_instituto_8 = 'Datafolha' and ano = 2022 and cargo = 'presidente'
