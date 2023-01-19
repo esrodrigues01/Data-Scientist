83dddf8c595107e54e3a5de27d3bb30b918d6708
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-19 16:23:49 -0400

    19/01

--- Projeto 3 - Análise Descritiva para geração de insight/src/main.py
+++ Projeto 3 - Análise Descritiva para geração de insight/src/main.py
@@ -2,10 +2,16 @@ import numpy as np
 import pandas as pd 
 import os
 
+
+##Lendo os dados CSV que estão na pasta dados
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR,'dados')
-
 files_names  = [i for i in os.listdir(DATA_DIR) if i.endswith('.csv')]
-
 for i in files_names:
     df = pd.read_csv(os.path.join(DATA_DIR,i))
+
+#Explorando o conjunto de dados
+print(df.info)
+print(df.isnull().sum())
+
+#Tratando dados ausentes, incorreto ou inválido
