d3cf74e8461dbbb28dcda374259ed4d295829765
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-23 15:06:30 -0400

    23/01

--- Projeto 5 - Analise de dados do ENEM/main.py
+++ Projeto 5 - Analise de dados do ENEM/main.py
@@ -1,1 +1,12 @@
 import os
+import pandas as pd
+
+
+##Lendo os dados CSV que estão na pasta dados
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR,'dados')
+files_names  = [i for i in os.listdir(DATA_DIR) if i.endswith('.csv')]
+for i in files_names:
+    microDadosEnem = pd.read_csv(os.path.join(DATA_DIR,i),sep=";", encoding="ISO-8859-1")
+print(microDadosEnem.head())
+print(microDadosEnem)
