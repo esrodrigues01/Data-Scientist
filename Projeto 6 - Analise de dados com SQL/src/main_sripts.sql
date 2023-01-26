f465ebcc336c18bb5e28a9e454e0c12904b71582
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-01-26 16:00:37 -0400

    26/01

--- Projeto 6 - Analise de dados com SQL/src/main_sripts.sql
+++ Projeto 6 - Analise de dados com SQL/src/main_sripts.sql
@@ -15,3 +15,20 @@ select
 	 * 
 from microdados m 
 where (instituto = 'Datafolha' or instituto = 'Ibope') and (ano > 2013) and (sigla_uf = 'MT')
+
+--Descobrir as siglas de partidos distintos que concorreram nas eleições a partir de 2014
+select
+	distinct trial_sigla_partido_22  
+from microdados m 
+where ano >= 2014 
+and (trial_sigla_partido_22 != 'sem partido' 
+and trial_sigla_partido_22 != 'N/A' 
+and trial_sigla_partido_22 != '* ' 
+and trial_sigla_partido_22 != '* T'
+and trial_sigla_partido_22 != '* TR'
+and trial_sigla_partido_22 != '* TRIA'
+and trial_sigla_partido_22 != '* TRIAL'
+and trial_sigla_partido_22 != '* TRIAL *'
+and trial_sigla_partido_22 != '* TRIAL * T'
+and trial_sigla_partido_22 != '* TRIAL * TR'
+and trial_sigla_partido_22 != '* TRIAL * TRI');
