25d767eee88e4cc7c55d9fa5708cfef115fe86ce
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-02-09 17:59:37 -0400

    09/02

--- Projeto 8 - Modelo de Deep Learning baseado na memória de Curto Prazo (LSTM)/main.py
+++ Projeto 8 - Modelo de Deep Learning baseado na memória de Curto Prazo (LSTM)/main.py
@@ -16,4 +16,25 @@ final = "2022-09-15"
 
 dados_acao = yf.download(acao,inicio,final)
 
-print(dados_acao)
+#pegando cotações de fechamento 
+
+cotacao_fechamento = dados_acao['Close'].to_numpy().reshape(-1,1)
+
+#print(cotacao_fechamento)
+
+#treinando 80% dos dados e 20% do teste
+tamanho_dados_treinamento =  int(len(cotacao_fechamento) * 0.8)
+
+#print(tamanho_dados_treinamento)
+
+#Escalar os dados 
+escalador = MinMaxScaler(feature_range=(0,1))
+dados_entre01_treinamento = escalador.fit_transform(cotacao_fechamento[0: tamanho_dados_treinamento, :])
+dados_entre01_teste = escalador.transform(cotacao_fechamento[tamanho_dados_treinamento:,:])
+
+dados_entre01 = list(dados_entre01_treinamento.reshape(
+    len(dados_entre01_treinamento))) + list(dados_entre01_teste.reshape(
+    len(dados_entre01_teste)))
+dados_entre01 = np.array(dados_entre01).reshape(len(dados_entre01), 1)
+
+print(dados_entre01)
