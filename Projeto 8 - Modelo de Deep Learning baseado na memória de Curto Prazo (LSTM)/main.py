ee542ddf46cf1fb9046dfbc0511a9965bf116b89
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-02-09 17:07:28 -0400

    09/02

--- Projeto 8 - Modelo de Deep Learning baseado na memória de Curto Prazo (LSTM)/main.py
+++ Projeto 8 - Modelo de Deep Learning baseado na memória de Curto Prazo (LSTM)/main.py
@@ -1,1 +1,11 @@
 import math
+from pandas_datareader import data as pdr 
+import numpy as np
+import pandas as pd 
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+from datetime import datetime
+from datetime import timedelta
+pd.options.mode.chained_assignment = None
