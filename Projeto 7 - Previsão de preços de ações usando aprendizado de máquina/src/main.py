176bc84c2945032c9d057a06f40dabece57ad41a
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-02-09 15:27:42 -0400

    09/02

---
+++ Projeto 7 - Previsão de preços de ações usando aprendizado de máquina/src/main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+
