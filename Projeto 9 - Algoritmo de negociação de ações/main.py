0816bd3883680820e2b178555ca8101c855010e0
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-02-13 13:10:22 -0400

    13/02

--- Projeto 9 - Algoritmo de negociação de ações/main.py
+++ Projeto 9 - Algoritmo de negociação de ações/main.py
@@ -14,14 +14,14 @@ data['momentum'] = data['Close'].pct_change()
 
 #Criando subplots para exibir o momento e compra/venda 
 figure = make_subplots(rows=2, cols=1)
-figure.add_trace(go.Scatter(x = data.index, y=data['Close'], name='Close Price'))
-figure.add_trace(go.Scatter(x=data.index, y= data['momentum'], name='Momentum', yaxis='y2'))
+figure.add_trace(go.Scatter(x = data.index, y=data['Close'], name='Preço de Fechamento'))
+figure.add_trace(go.Scatter(x=data.index, y= data['momentum'], name='Momento', yaxis='y2'))
 
 #Adicionando sinais de COMPRA e VENDA 
 figure.add_trace(go.Scatter(x = data.loc[data['momentum'] > 0 ].index, y= data.loc[data['momentum'] > 0] ['Close'], mode='markers', name = 'Compra', marker=dict(color='green', symbol = 'triangle-up')))
 
 figure.add_trace(go.Scatter(x = data.loc[data['momentum'] < 0 ].index, y= data.loc[data['momentum'] < 0] ['Close'], mode='markers', name = 'Venda', marker=dict(color='red', symbol = 'triangle-down')))
 
-figure.update_layout(title = 'Algoritmo de negociação usando a estratégia do momento', xaxis_title = 'Date', yaxis_title = 'Price')
+figure.update_layout(title = 'Algoritmo de negociação usando a estratégia do momento', xaxis_title = 'Data', yaxis_title = 'Preço')
 figure.update_yaxes(title = "Momentum", secondary_y = True)
 figure.show()
