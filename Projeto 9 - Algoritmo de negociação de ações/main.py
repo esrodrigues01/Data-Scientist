67b4b9504c88a41a432a5c05744d786eb7c340c0
Emerson da Silva Rodrigues <emerson_rodrigues01@yahoo.com>
2023-02-13 13:07:33 -0400

    13/02

--- Projeto 9 - Algoritmo de negociação de ações/main.py
+++ Projeto 9 - Algoritmo de negociação de ações/main.py
@@ -5,9 +5,9 @@ import plotly.express as px
 import yfinance as yf
 
 #Obtendo dados das ações da Apple no yahoo finance
-stock = yf.Ticker("AAPL")
+stock = yf.Ticker("PETR4.SA")
 data = stock.history(period = "1y")
-print(data.head())
+#print(data.head())
 
 #Calculadora de momento 
 data['momentum'] = data['Close'].pct_change()
@@ -18,3 +18,10 @@ figure.add_trace(go.Scatter(x = data.index, y=data['Close'], name='Close Price')
 figure.add_trace(go.Scatter(x=data.index, y= data['momentum'], name='Momentum', yaxis='y2'))
 
 #Adicionando sinais de COMPRA e VENDA 
+figure.add_trace(go.Scatter(x = data.loc[data['momentum'] > 0 ].index, y= data.loc[data['momentum'] > 0] ['Close'], mode='markers', name = 'Compra', marker=dict(color='green', symbol = 'triangle-up')))
+
+figure.add_trace(go.Scatter(x = data.loc[data['momentum'] < 0 ].index, y= data.loc[data['momentum'] < 0] ['Close'], mode='markers', name = 'Venda', marker=dict(color='red', symbol = 'triangle-down')))
+
+figure.update_layout(title = 'Algoritmo de negociação usando a estratégia do momento', xaxis_title = 'Date', yaxis_title = 'Price')
+figure.update_yaxes(title = "Momentum", secondary_y = True)
+figure.show()
